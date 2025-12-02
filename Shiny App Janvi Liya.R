86ab2f2e4f605cd7d0a1dd5c969ace9ab318578c
Janvi G <janvigb51@gmail.com>
2025-12-02 19:35:53 +0000

    2nd dec 7.35 pm

--- Shiny App Janvi Liya.R
+++ Shiny App Janvi Liya.R
@@ -6,10 +6,12 @@ if (!require("shiny")) install.packages("shiny")
 if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("DynNom")) install.packages("DynNom")
 if (!require("shinydashboard")) install.packages("shinydashboard")
+if (!require("shinyWidgets")) install.packages("shinyWidgets")
 library(shiny)
 library(tidyverse)
 library(DynNom)
 library(shinydashboard)
+library(shinyWidgets)
 
 # Import the DIG dataset
 #setwd("C:/Users/janvi/Desktop/shiny_assignment_janvi_liya/R Assignment 5/")
@@ -28,17 +30,19 @@ dig_data <- dig %>%
 ui <- dashboardPage(
   dashboardHeader(title = "Digitalis Investigation Group", titleWidth = 450),
   dashboardSidebar(
-    radioButtons(inputId = "treatment", label = "Select Treatment:", choices = c("treatment", "placebo")),
-    sliderInput(inputId = "age", label = "Select Age:", min = 20, max = 90, value = c(60, 70)),
-    radioButtons(inputId = "sex", label = "Select Gender:", choices = c("male", "female")),
-    sliderInput(inputId = "bmi", label = "Select Body Mass Index:", min = 10, max = 65, value = c(10, 65)),
-    sliderInput(inputId = "diabp", label = "Select Diastolic BP:", min = 20, max = 190, value = c(60, 90)),
-    sliderInput(inputId = "sysbp", label = "Select Systolic BP:", min = 70, max = 220, value = c(100, 140)),
-    radioButtons(inputId = "cvd", label = "Select Cardiovascular Disease:", choices = c("NO", "YES")),
-    radioButtons(inputId = "hosp", label = "Select Hospitalization:", choices = c("NO", "YES")),
-    sliderInput(inputId = "hospdays", label = "Select Hospitalization Days:", min = 0, max = 1800, value = c(30, 90)),
-    radioButtons(inputId = "death", label = "Select Vital Status:", choices = c("Death", "Alive")),
-    sliderInput(inputId = "deathdays", label = "Select Death Day:", min = 0, max = 1800, value = c(1300, 1600))),
+    dropdownButton(
+      inputId = "filters", label = "Show filters", icon = icon("sliders"), circle = FALSE, status = "primary", width = "320px",
+      checkboxGroupInput(inputId = "treatment", label = "Select Treatment:", choices = c("treatment", "placebo"), selected = c("treatment", "placebo")),
+      sliderInput(inputId = "age", label = "Select Age:", min = 20, max = 90, value = c(60, 70)),
+      checkboxGroupInput(inputId = "sex", label = "Select Gender:", choices = c("male", "female"), selected = c("male", "female")),
+      sliderInput(inputId = "bmi", label = "Select BMI (Body Mass Index):", min = 10, max = 65, value = c(10, 65)),
+      sliderInput(inputId = "diabp", label = "Select Diastolic BP:", min = 20, max = 190, value = c(60, 90)),
+      sliderInput(inputId = "sysbp", label = "Select Systolic BP:", min = 70, max = 220, value = c(100, 140)),
+      checkboxGroupInput(inputId = "cvd", label = "Select Cardiovascular Disease:", choices = c("NO", "YES"), selected = c("NO", "YES")),
+      checkboxGroupInput(inputId = "hosp", label = "Select Hospitalization:", choices = c("NO", "YES"), selected = c("NO", "YES")),
+      sliderInput(inputId = "hospdays", label = "Select Hospitalization Days:", min = 0, max = 1800, value = c(30, 90)),
+      checkboxGroupInput(inputId = "death", label = "Select Vital Status:", choices = c("death", "alive"), selected = c("death", "alive")),
+      sliderInput(inputId = "deathdays", label = "Select Death Day:", min = 0, max = 1800, value = c(1300, 1600)))),
   dashboardBody(
     plotOutput("plot1"),
   ))
@@ -46,30 +50,30 @@ ui <- dashboardPage(
 server <- function(input, output) {
   dig_1 <- reactive({
     dig_data %>%
-      filter(TRTMT == input$treatment) %>%
-      filter(AGE >= input$age[1], AGE <=input$age[2]) %>%
-      filter(SEX == input$sex) %>%
+      filter(TRTMT %in% input$treatment) %>%
+      filter(AGE  >= input$age[1],  AGE  <= input$age[2]) %>%
+      filter(SEX %in% input$sex) %>%
       filter(BMI  >= input$bmi[1],  BMI  <= input$bmi[2]) %>%
-      filter(DIABP >= input$diabp[1], DIABP <= input$diabp[2]) %>%
-      filter(SYSBP >= input$sysbp[1], SYSBP <= input$sysbp[2]) %>%
-      filter(CVD == input$cvd) %>%
-      filter(HOSP == input$hosp) %>%
-      filter(HOSPDAYS >= input$hospdays[1], HOSPDAYS <= input$hospdays[2]) %>%
-      filter(DEATH == input$death) %>%
-      filter(DEATHDAY >= input$deathdays[1], DEATHDAY <= input$deathdays[2])
+      filter(DIABP>= input$diabp[1],DIABP<= input$diabp[2]) %>%
+      filter(SYSBP>= input$sysbp[1],SYSBP<= input$sysbp[2]) %>%
+      filter(CVD %in% input$cvd) %>%
+      filter(HOSP %in% input$hosp) %>%
+      filter(DEATH %in% input$death)
   })
-  output$plot1 <- renderPlot({ 
-    ggplot(data = dig_1(), mapping = aes(x = TRTMT, fill = SEX)) +
-      geom_bar(alpha = 0.4) +
-      scale_fill_manual(values = c("male" = "darkblue", "female" = "maroon")) +
-      labs(title = "Summary of patients between treatment and placebo groups",
-           fill = "Sex",
-           x = "Treatment",
-           y= "Patient Count") +
-      theme_minimal()
+  output$plot1 <- renderPlot({
+    df <- dig_1()
+    ggplot(data = df, mapping = aes(x = AGE, y = BMI, colour = TRTMT, shape = SEX)) +
+      geom_point(size = 2, alpha = 0.7) +
+      scale_color_manual(values = c("placebo" = "darkblue", "treatment" = "maroon")) +
+      scale_shape_manual(values = c("male" = 15, "female" = 17)) +
+      labs(title = "Age vs BMI",
+           colour = "Treatment (by color)",
+           shape = "Sex (by shape)",
+           x = "Age",
+           y= "BMI")
   })
 }
 
 shinyApp(ui = ui, server = server)
-# Doesn't work yet but I'll try again tomorrow! fingers crossed!
 
+## Still needs a lot of editing!
